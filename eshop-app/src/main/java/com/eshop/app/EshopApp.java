d1f9e6f4424da988dbadc05401482e71edece2c9
Ammar Al-Sharif <aalshareef57@gmail.com>
2020-08-10 23:22:08 +0300

    initial commit

---
+++ eshop-app/src/main/java/com/eshop/app/EshopApp.java
@@ -0,0 +1,11 @@
+package com.eshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EshopApp.class, args);
+    }
+}
