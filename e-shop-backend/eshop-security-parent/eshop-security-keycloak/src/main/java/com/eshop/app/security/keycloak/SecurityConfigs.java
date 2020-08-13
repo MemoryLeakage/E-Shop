3e91b2fe310bba8ce9003b0a076c8d0c1c393b62
Abdelrahman <aboodthafery@yahoo.com>
2020-08-13 23:40:38 +0300

    feat: connect backend to keycloak

---
+++ e-shop-backend/eshop-security-parent/eshop-security-keycloak/src/main/java/com/eshop/app/security/keycloak/SecurityConfigs.java
@@ -0,0 +1,64 @@
+package com.eshop.app.security.keycloak;
+
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.keycloak.adapters.springsecurity.AdapterDeploymentContextFactoryBean;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+@Configuration
+@EnableWebSecurity
+@KeycloakConfiguration
+public class SecurityConfigs extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/ping").authenticated()
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    protected AdapterDeploymentContext adapterDeploymentContext() throws Exception {
+        AdapterDeploymentContextFactoryBean factoryBean;
+        DefaultResourceLoader defaultResourceLoader = new DefaultResourceLoader();
+        Resource keycloakConfigFileResource = defaultResourceLoader.getResource("/WEB-INF/keycloak.json");
+        factoryBean = new AdapterDeploymentContextFactoryBean(keycloakConfigFileResource);
+        factoryBean.afterPropertiesSet();
+        return factoryBean.getObject();
+
+    }
+}
