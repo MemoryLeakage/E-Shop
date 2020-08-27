b7de1e3e53523a6b3193a2ab846c7f2865b7066b
Abdelrahman <aboodthafery@yahoo.com>
2020-08-27 22:14:30 +0300

    feat: keycloak event listener SPI

--- keycloak/users-event-listener/src/main/java/com/eshop/keycloak/spis/UserEventListenerProviderFactory.java
+++ keycloak/users-event-listener/src/main/java/com/eshop/keycloak/spis/UserEventListenerProviderFactory.java
@@ -1,4 +1,37 @@
 package com.eshop.keycloak.spis;
 
-public class UserEventLIstenerProviderFactory {
+import org.keycloak.Config;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class UserEventListenerProviderFactory implements EventListenerProviderFactory {
+
+    private static EventListenerProvider eventListenerProvider;
+
+    @Override
+    public EventListenerProvider create(KeycloakSession keycloakSession) {
+        return eventListenerProvider = new UserEventListenerProvider(keycloakSession);
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "User-Event-Listener";
+    }
 }
